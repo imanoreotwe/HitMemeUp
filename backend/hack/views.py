3e9ccf4c59c312b265c727ccc94484a8bfbc17c1
imanoreotwe <35ded66b@opayq.com>
2019-08-15 12:06:37 -0600

    Update views.py

--- backend/hack/views.py
+++ backend/hack/views.py
@@ -21,13 +21,13 @@ from urllib import parse
 #client_id = config.get('credentials', 'client_id')
 #client_secret = config.get('credentials', 'client_secret')
 
-client_id = "741a70c559947a2"
-client_secret = "c97cf173d6c7bfd71f7486cbd23d8f98a9e578b9"
+client_id = ""
+client_secret = ""
 
 client = ImgurClient(client_id, client_secret)
 
-twi_account_sid = 'ACd5b96657935efd3a8d35495e2a9b1ea0'
-twi_auth_token = '798eecacd63248f6e604a51a274fb916'
+twi_account_sid = ''
+twi_auth_token = ''
 twi_client = Client(twi_account_sid, twi_auth_token)
 
 @app.route("/sms", methods=['GET', 'POST'])
@@ -56,26 +56,26 @@ def sms_ahoy_reply():
                     print(image_link)
                     message = twi_client.messages \
                             .create(
-                                    from_='+17205130277',
+                                    from_='',
                                     to=request.values['From'],
                                     media_url=image_link
                             )
             except:
                 message = twi_client.messages.create(
-                                    from_='+17205130277',
+                                    from_='',
                                     to=request.values['From'],
                                     body='An unexpected error occured.'
                 )
         elif fields[0].lower() == 'gif':
             # return a gif related to the keyword
             req = request.values['Body'].replace("meme", "", 1)
-            query = {'q': req, 'apikey': 'dc6zaTOxFJmzC'}
-            headers = {'X-RapidAPI-Key': '0b7e7db0dfmshddfdc1e9523edf6p1c40d5jsn42179db7b62e'}
+            query = {'q': req, 'apikey': ''}
+            headers = {'X-RapidAPI-Key': ''}
             r = requests.get('https://giphy.p.rapidapi.com/v1/gifs/search?' + urllib.parse.urlencode(query), headers=headers)
             items = r.json()['data']
             item_index = random.randint(0, len(items)-1)
             message = twi_client.messages.create(
-                                from_='+17205130277',
+                                from_='',
                                 to=request.values['From'],
                                 media_url=items[item_index]['images']['original']['url']
             )
@@ -83,17 +83,17 @@ def sms_ahoy_reply():
         elif fields[0].lower() == 'insult':
             # return a random spotify song
             query = {'mode': 'random'}
-            headers = {'X-RapidAPI-Key': '0b7e7db0dfmshddfdc1e9523edf6p1c40d5jsn42179db7b62e'}
+            headers = {'X-RapidAPI-Key': ''}
             r = requests.get('https://lakerolmaker-insult-generator-v1.p.rapidapi.com/?' + urllib.parse.urlencode(query), headers=headers)
             message = twi_client.messages.create(
-                                from_='+17205130277',
+                                from_='',
                                 to=request.values['From'],
                                 body=r.text
             )
 
     else:
         message = twi_client.messages.create(
-                            from_='+17205130277',
+                            from_='',
                             to=request.values['From'],
                             body='No results found.'
                             )
